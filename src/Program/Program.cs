1655b977772e4e2b52bf14cea3f87e6021a96adb
Agus1441 <agustin.schlechter@correo.ucu.ed>
2023-10-09 09:49:18 -0300

    Solución

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,31 +1,24 @@
-﻿using System;
-using RoleplayGame;
+using System;
 
-namespace Program
+public class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            SpellsBook book = new SpellsBook();
-            book.AddSpell(new SpellOne());
-            book.AddSpell(new SpellOne());
+        Enano enano1 = new Enano("Gimli", 120, 18, 12, 8, 0);
+        Mago mago1 = new Mago("Gandalf", 80, 10, 8, 20, 0);
+        Elfo elfo1 = new Elfo("Legolas", 100, 20, 10, 15, 0);
 
-            Wizard gandalf = new Wizard("Gandalf");
-            gandalf.AddItem(book);
+        Enemigo enemigo1 = new Enemigo("Orco1","Orco", 80, 15, 8, 14, 5);
+        Enemigo enemigo2 = new Enemigo("Goblin1","Goblin", 100, 12, 6, 21, 3);
 
-            Dwarf gimli = new Dwarf("Gimli");
+        List<IPersonaje> heroes = new List<IPersonaje> { enano1, mago1, elfo1 };
+        List<Enemigo> enemies = new List<Enemigo> { enemigo1, enemigo2 };
 
-            Console.WriteLine($"Gimli has ❤️ {gimli.Health}");
-            Console.WriteLine($"Gandalf attacks Gimli with ⚔️ {gandalf.AttackValue}");
+        Encuentro encounter = new Encuentro(heroes, enemies);
 
-            gimli.ReceiveAttack(gandalf.AttackValue);
-
-            Console.WriteLine($"Gimli has ❤️ {gimli.Health}");
-
-            gimli.Cure();
-
-            Console.WriteLine($"Someone cured Gimli. Gimli now has ❤️ {gimli.Health}");
-        }
+        encounter.Pelear();
     }
 }
+
+    
+
